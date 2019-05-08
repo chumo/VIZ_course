86673b23e04ee3108aea184d4f03421457edcaec
chumo <jmb.jesus@gmail.com>
2019-05-09 00:18:34 +0200

    DSR-18 update: added plotly section notebooks

--- PLOTLY/solutions/solution_0.py
+++
@@ -1,9 +0,0 @@
-layout = go.Layout(
-    title='Some Experiment',
-    xaxis = dict(title = 'Some independent variable'),
-    yaxis = dict(title = 'Some dependent variable')
-    )
-
-fig = go.Figure(data=data, layout=layout)
-
-py.offline.iplot(fig)
--- PLOTLY/solutions/solution_1.py
+++
@@ -1,9 +0,0 @@
-trace2 = go.Bar( x=['giraffes', 'orangutans', 'monkeys'], y=[12, 18, 29], name='LA Zoo' )
-
-data = [trace1, trace2]
-
-layout = go.Layout(barmode='group') #try out stack here
-
-fig = go.Figure(data=data, layout=layout)
-
-py.offline.iplot(fig)
